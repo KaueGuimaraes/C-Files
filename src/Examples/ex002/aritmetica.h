9545b0e829bab71fae36ebb6c14d6f663cf7499c
KaueGuimaraes <kauebrunoguima@gmail.com>
2022-02-19 19:16:51 -0300

    .

---
+++ src/Examples/ex002/aritmetica.h
@@ -0,0 +1,12 @@
+int soma(int a, int b) {
+    return a + b;
+}
+int subtracao(int a, int b) {
+    return a - b;
+}
+int produto(int a, int b) {
+    return a * b;
+}
+int divisao(int a, int b) {
+    return b / a; // Por algum motivo tive que inverter pra ficar na ordem certa, estranho
+}
